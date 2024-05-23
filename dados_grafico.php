4a4776ac00433e6590da522e4391f4b04f8d71d3
TassianaSgarbi <tassiana.sgarbi@gmail.com>
2024-05-22 22:02:14 -0300

    grafico30 louco

--- dados_grafico.php
+++ dados_grafico.php
@@ -10,13 +10,28 @@ if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
+if ($_GET['zone'] == 'Santos') {
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY) GROUP BY DATA;";
+
+} else if ($_GET['zone'] == 'Zona da Orla e intermediária'){
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY ) AND ZONAS = 'Zona da Orla e intermediária' GROUP BY DATA;";
+
+} else if ($_GET['zone'] == 'Região Central Histórica'){
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY ) AND ZONAS = 'Região Central Histórica' GROUP BY DATA;";
+
+}  else if ($_GET['zone'] == 'Zona dos Morros'){
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY ) AND ZONAS = 'Zona dos Morros' GROUP BY DATA;";
+} 
+
+else if ($_GET['zone'] == 'Zona Noroeste'){
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY ) AND ZONAS = 'Zona Noroeste' GROUP BY DATA;";
+
+} else if ($_GET['zone'] == 'Área Continental'){
+    $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY ) AND ZONAS = 'Área Continental' GROUP BY DATA;";
+}
+
 // Seleciona o número de demandas executadas por dia nos últimos 30 dias
-$sql = "
-SELECT DAY(Data) as Dia, COUNT(*) as Quantidade
-FROM Solicitacao_de_Demandas
-WHERE Status_Demanda = 'Demandas Executadas' AND Data >= CURDATE() - INTERVAL 30 DAY
-GROUP BY Dia
-ORDER BY Dia";
+// $sql = "SELECT COUNT(*) AS qtde, DATA FROM `solicitacao_de_demandas` WHERE DATA > DATE_SUB(CURDATE(), INTERVAL 30 DAY) GROUP BY DATA;";
 
 $result = $conn->query($sql);
 
@@ -29,4 +44,3 @@ $conn->close();
 
 header('Content-Type: application/json');
 echo json_encode($data);
-?>
